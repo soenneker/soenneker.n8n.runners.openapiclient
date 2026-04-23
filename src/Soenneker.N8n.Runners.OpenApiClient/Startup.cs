1c5c1c60475fdbb144f9787cded1230af5751425
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-23 11:36:21 -0500

    lifetime issues

--- src/Soenneker.N8n.Runners.OpenApiClient/Startup.cs
+++ src/Soenneker.N8n.Runners.OpenApiClient/Startup.cs
@@ -21,10 +21,10 @@ public static class Startup
     public static IServiceCollection SetupIoC(this IServiceCollection services)
     {
         services.AddHostedService<ConsoleHostedService>()
-                .AddScoped<IFileOperationsUtil, FileOperationsUtil>()
-                .AddRunnersManagerAsScoped()
-                .AddPlaywrightInstallationUtilAsScoped()
-                .AddKiotaUtilAsScoped();
+                .AddSingleton<IFileOperationsUtil, FileOperationsUtil>()
+                .AddRunnersManagerAsSingleton()
+                .AddPlaywrightInstallationUtilAsSingleton()
+                .AddKiotaUtilAsSingleton();
 
         return services;
     }
